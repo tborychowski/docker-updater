67c829008ab6572300768300e03fa43e0d1ecee9
Tomasz Borychowski <tborychowski@gmail.com>
2020-08-13 21:20:58 +0100

    twks

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ const chalk = require('chalk');
 const ora = require('ora');
 const {docker} = require('./lib');
 
+const plur = no => no === 1 ? '' : 's';
 
-function run () {
+function run (params) {
 	const spinner = ora('Checking containers...').start();
 	docker
 		.checkForUpdates()
 		.then(res => {
-			if (res.length === 0) spinner.succeed(' nothing to update!');
-			else spinner.succeed(` Found ${res.length} container${res.length === 1 ? '' : 's'} to update:`);
+			let msg = '';
+			if (!res || !res.length) msg = ' no containers found!';
+			else {
+				const toUpd = res.filter(i => i.hasUpdate);
+				if (params.show_all) msg = ` Found ${res.length} container${plur(res.length)}:`;
+				else msg = ` Found ${toUpd.length} container${plur(toUpd.length)} to update:`;
+				if (!params.show_all) res = toUpd;
+			}
+			spinner.succeed(msg);
+
 			res.forEach(c => {
 				if (!c) return;
 				if (!c.hasUpdate) return;
@@ -22,7 +31,8 @@ function run () {
 }
 
 
-const args = new Args('docker updater', '1.0', 'Check for updates for an docker image.');
-args.add({ name: 'pull', switches: [ '-p', '--pull' ], desc: 'Pull the image' });
+const args = new Args('docker updater', '1.1', 'Check for updates for running docker containers.');
+// args.add({ name: 'pull', switches: [ '-p', '--pull' ], desc: 'Pull the image' });
+args.add({ name: 'show_all', switches: [ '-a', '--all' ], desc: 'Show all containers' });
 args.add({ name: 'image', desc: 'Image name, e.g. tborychowski/sermon' });
 if (args.parse()) run(args.params);
