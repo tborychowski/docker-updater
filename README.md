3e64670dfa48ceadf45f2c9bf5a1bff0c20a405a
Dziad Borowy <github@borychowski.org>
2022-09-19 17:29:41 +0100

    proper working version

--- README.md
+++ README.md
@@ -1,15 +1,38 @@
 # docker updater
 Check for updates for images of running containers.
 
+
+## Requirements
+1. NodeJS v18+ installed
+2. docker CLI installed
+
+
 ## Usage
 ```sh
-docker-updater
+node index.js  # can be aliased to e.g. `dupd`
 ```
 
 
-## Docker v2 API
-- https://docs.docker.com/registry/spec/api/
 
+## Setup for GitHub & GitLab
+
+### If using GitHub (ghcr.io) registry - a token is required:
+1. Create a new token: https://github.com/settings/tokens with `read:packages` scope.
+2. Add `GH_TOKEN=<token>` to `.env` file
+
+
+### If using GitLab registry - a token is required:
+1. Create a new token: https://gitlab.com/-/profile/personal_access_tokens with `read_api` and `read_registry` scopes.
+2. Add `GITLAB_TOKEN=<token>` to `.env` file
+
+
+## What's wrong with WatchTower?
+WatchTower and Diun (I didn't find anything else) have a workflow that I didn't like:<br>
+- blind pull everything -> diff hashes -> notify or auto-update<br>
+
+I want to be able to check for updates without pulling anything and at the time I have time to update manually. Like a typical package manager.<br>
+
+This script checks for updates using registry API and check the dates and hashes.
 
 ## Inspiration
 - https://github.com/al4/docker-registry-list
--- index.js
+++ index.js
@@ -6,6 +6,22 @@ import { checkForUpdates } from './lib/index.js';
 
 const plur = no => no === 1 ? '' : 's';
 
+function printResult (res) {
+	if (!res || !res.length) return;
+
+	res.forEach(c => {
+		if (!c) return;
+		let info = '';
+
+		if (c.error) info = chalk.red(c.error || '');
+		else info = c.hasUpdate ? chalk.yellow('Update found!') : chalk.green('Up-to-date!');
+
+		console.log('-', c.Names + ':', info);
+	});
+
+}
+
+
 function run (params) {
 	const spinner = ora('Checking containers...').start();
 	checkForUpdates()
@@ -14,21 +30,20 @@ function run (params) {
 			if (!res || !res.length) msg = ' no containers found!';
 			else {
 				res.sort((a, b) => a.Names.localeCompare(b.Names));
+				const withError = res.filter(i => i.error);
 				const toUpd = res.filter(i => i.hasUpdate);
+				const upToDate = res.filter(i => !i.hasUpdate && !i.error);
+
 				msg = ` Found ${res.length} container${plur(res.length)}, `;
 
 				if (toUpd.length) msg += chalk.yellow(toUpd.length + ' to update:');
-				else msg += chalk.green('up-to-date') + (params.show_all ? ':' : '.');
+				else msg += chalk.green(upToDate.length + ' up-to-date') + (params.show_all ? ':' : '.');
 
-				if (!params.show_all) res = toUpd;
+				if (!params.show_all) res = [...toUpd, ...withError];
 			}
 
 			spinner.succeed(msg);
-			if (res && res.length) res.forEach(c => {
-				if (!c) return;
-				const upd = c.hasUpdate ? chalk.yellow('Update found!') : chalk.green('Up-to-date!');
-				console.log('-', c.Names + ':', upd);
-			});
+			printResult(res);
 		})
 		.catch(e => spinner.fail(e));
 }
